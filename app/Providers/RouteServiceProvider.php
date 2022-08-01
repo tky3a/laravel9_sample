65def447536b578d6d467b87e1f80a0aae1f5ec8
tky <takayamac@icloud.com>
2022-08-01 21:18:40 +0900

    ログイン後の遷移をtweet画面に変更

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,8 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/dashboard';
+    // public const HOME = '/dashboard';
+    public const HOME = '/tweet'; // ログイン後の遷移をtweet画面に変更
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
