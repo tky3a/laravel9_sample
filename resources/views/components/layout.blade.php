857044e81ed2a367d13c284e7c03ffbec841898f
tky <takayamac@icloud.com>
2022-08-02 12:11:16 +0900

    laravel-viteでtailwindを導入

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="ja">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, minimum-scale=1.0">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <title>{{ $title ?? 'つぶやきアプリ' }}</title>
+</head>
+
+<body class="bg-gray-100 pl-2 pr-2">
+    {{ $slot }}
+</body>
+
+</html>
