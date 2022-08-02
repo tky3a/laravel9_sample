782f044e0430a58d669ef53ef307139912ff2a96
tky <takayamac@icloud.com>
2022-08-02 09:10:58 +0900

    authでログインしていないとアクセスできないよう対応

--- routes/web.php
+++ routes/web.php
@@ -20,12 +20,13 @@ Route::get('/', function () {
 Route::get('/sample', [\App\Http\Controllers\sample\IndexController::class, 'show']);
 Route::get('/sample/{id}', [\App\Http\Controllers\sample\IndexController::class, 'showId']);
 Route::get('/tweet', \App\Http\Controllers\Tweet\IndexController::class)->name('tweet.index');
-Route::post('/tweet/create', \App\Http\Controllers\Tweet\CreateController::class)
-    ->middleware('auth') // ログインしていないユーザーが書き込みするとログインにリダイレクトする
-    ->name('tweet.create');
-Route::get('/tweet/update/{tweetId}', \App\Http\Controllers\Tweet\Update\IndexController::class)->name('tweet.update.index');
-Route::put('/tweet/update/{tweetId}', \App\Http\Controllers\Tweet\Update\PutController::class)->name('tweet.update.put');
-Route::delete('/tweet/delete/{tweetId}', \App\Http\Controllers\Tweet\DeleteController::class)->name('tweet.delete');
+Route::middleware('auth')->group(function () {
+    Route::post('/tweet/create', \App\Http\Controllers\Tweet\CreateController::class)
+        ->name('tweet.create');
+    Route::get('/tweet/update/{tweetId}', \App\Http\Controllers\Tweet\Update\IndexController::class)->name('tweet.update.index');
+    Route::put('/tweet/update/{tweetId}', \App\Http\Controllers\Tweet\Update\PutController::class)->name('tweet.update.put');
+    Route::delete('/tweet/delete/{tweetId}', \App\Http\Controllers\Tweet\DeleteController::class)->name('tweet.delete');
+});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
