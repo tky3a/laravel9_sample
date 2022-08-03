b067b5874732e2716a779f80f2776ad23ded9058
tky <takayamac@icloud.com>
2022-08-03 21:08:40 +0900

    メールの形式をリッチに修正（ボタンとパネルで表示）

--- resources/views/email/new_user_introduction.blade.php
+++ resources/views/email/new_user_introduction.blade.php
@@ -3,5 +3,12 @@
 
   {{ $toUser->name }}さんこんにちは！
 
-  新しく{{ $newUser->name }}さんが参加しました！
+  @component('mail::panel')
+    新しく{{ $newUser->name }}さんが参加しました！
+  @endcomponent
+
+  @component('mail::button', ['url' => route('tweet.index')])
+    つぶやきを見にいく
+  @endcomponent
+
 @endcomponent
