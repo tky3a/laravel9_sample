7b3b4e88faa124c73f8814a8c54f572f966eac53
tky <takayamac@icloud.com>
2022-08-03 18:51:18 +0900

    successメッセージ表示コンポーネント

---
+++ resources/views/components/alert/success.blade.php
@@ -0,0 +1,11 @@
+<div class="tet-white mt-1 mb-2 flex w-full items-center bg-green-500 p-2 leading-none lg:inline-flex lg:rounded-full"
+  role="alert">
+  <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+    <path fill-rule="evenodd"
+      d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+      clip-rule="evenodd" />
+  </svg>
+  <span class="mr-2 flex-auto pl-1 text-left font-semibold">
+    {{ $slot }}
+  </span>
+</div>
