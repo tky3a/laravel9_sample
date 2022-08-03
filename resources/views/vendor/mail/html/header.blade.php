53eb7466c50102e41355db81744d2f67f640bee3
tky <takayamac@icloud.com>
2022-08-03 21:22:49 +0900

    送信mailのデザインを変更

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,6 +1,7 @@
 <tr>
   <td class="header">
     <a href="{{ $url }}" style="display: inline-block;">
+      つぶやきアプリ
       @if (trim($slot) === 'Laravel')
         <img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
       @else
