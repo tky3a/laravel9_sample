26d6fd2863260325fda72c94d22bd32e96b67237
tky <takayamac@icloud.com>
2022-08-03 10:54:13 +0900

    つぶやきの一覧が表示されるよう対応

--- resources/views/tweet/index.blade.php
+++ resources/views/tweet/index.blade.php
@@ -2,47 +2,20 @@
 <html lang="ja">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, minimum-scale=1.0">
-    <title>つぶやきアプリ</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, minimum-scale=1.0">
+  <title>つぶやきアプリ</title>
 </head>
 
-<body>
-    <h1>つぶやきアプリ</h1>
-    @auth
-        <div>
-            <p>投稿フォーム</p>
-            <form action="{{ route('tweet.create') }}" method="post">
-                @csrf
-                <label for="tweet-content">つぶやき</label>
-                <span>140文字まで</span>
-                <textarea name="tweet" id="tweet-content" type="text" placeholder="つぶやきを入力してください。"></textarea>
-                @error('tweet')
-                    <p style="color: red;">{{ $message }}</p>
-                @enderror
-                <button type="submit">投稿</button>
-            </form>
-            <div>
-                @foreach ($tweets as $tweet)
-                    <details>
-                        <summary>{{ $tweet->content }} by {{ $tweet->user->name }}</summary>
-                        @if ($tweet->user_id === auth()->id())
-                            <div>
-                                <a href="{{ route('tweet.update.index', ['tweetId' => $tweet->id]) }}">編集</a>
-                                <form action="{{ route('tweet.delete', ['tweetId' => $tweet->id]) }}" method="post">
-                                    @method('DELETE')
-                                    @csrf
-                                    <button type="submit">削除</button>
-                                </form>
-                            </div>
-                        @else
-                            編集できません
-                        @endif
-                    </details>
-                @endforeach
-            </div>
-        </div>
-    @endauth
-</body>
+{{-- <body> --}}
+<x-layout title="TOP | つぶやき">
+  <x-layout.single>
+    <h2 class="mt-8 mb-8 text-center text-4xl font-bold text-blue-500">
+      つぶやきアプリ
+    </h2>
+    <x-tweet.form.post></x-tweet.form.post>
+    <x-tweet.list :tweets="$tweets"></x-tweet.list>
+  </x-layout.single>
+</x-layout>
 
 </html>
