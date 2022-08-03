7d7c7f0e8da18421e3c4242a75d1ebddf38fd572
tky <takayamac@icloud.com>
2022-08-03 18:52:39 +0900

    変更画面をコンポーネントで組み合わせ

--- resources/views/tweet/update.blade.php
+++ resources/views/tweet/update.blade.php
@@ -1,35 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
+<x-layout title="編集 | つぶやきアプリ">
+  <x-layout.single>
+    <h2 class="mt-8 mb-8 text-center text-4xl font-bold text-blue-500">つぶやきアプリ</h2>
+    @php
+      $breadcrumbs = [['href' => route('tweet.index'), 'label' => 'TOP'], ['href' => '#', 'label' => ' 編集']];
+    @endphp
+    <x-element.breadcrumbs :breadcrumbs="$breadcrumbs">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>つぶやきアプリ</title>
-</head>
-
-<body>
-    <h1>つぶやきを編集する</h1>
-    <div>
-        <a href="{{ route('tweet.index') }}">戻る</a>
-        <p>投稿フォーム</p>
-        @if (session('feedback.success'))
-            <p style="color: green;">{{ session('feedback.success') }}</p>
-        @endif
-        <form action="{{ route('tweet.update.put', ['tweetId' => $tweet->id]) }}" method="post">
-            @method('PUT')
-            @csrf
-            <label for="tweet-content">つぶやき</label>
-            <span>140文字まで</span>
-            <textarea name="tweet" id="tweet-content" cols="30" rows="10" placeholder="つぶやきを入力してください。">
-                {{ $tweet->content }}
-            </textarea>
-            @error('tweet')
-                <p style="color: red;">{{ $message }}</p>
-            @enderror
-            <button type="submit">編集</button>
-        </form>
-    </div>
-</body>
-
-</html>
+    </x-element.breadcrumbs>
+    <x-tweet.form.put :tweet="$tweet"></x-tweet.form.put>
+  </x-layout.single>
+</x-layout>
