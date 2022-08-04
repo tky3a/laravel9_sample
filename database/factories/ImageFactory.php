5b018ca7ca070e04727335a450251a71adfaae3a
tky <takayamac@icloud.com>
2022-08-04 20:32:08 +0900

    Factory修正

--- database/factories/ImageFactory.php
+++ database/factories/ImageFactory.php
@@ -3,6 +3,8 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Storage;
+use Faker\Generator as Faker;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Image>
@@ -16,8 +18,13 @@ class ImageFactory extends Factory
      */
     public function definition()
     {
-        return [
-            //
-        ];
+        // ディレクトリがなければ作成
+        if (Storage::exists('public/images')) {
+            Storage::makeDirectory('public/images');
+        }
+        // fakerでimageが使えないバグが出てそう？
+        // return [
+        //     'name' => $this->faker->image(storage_path('app/public/images'), 640, 480, null, false)
+        // ];
     }
 }
--- database/seeders/TweetsSeeder.php
+++ database/seeders/TweetsSeeder.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
 use App\Models\Tweet;
+use App\Models\Image;
 
 class TweetsSeeder extends Seeder
 {
@@ -23,5 +24,12 @@ class TweetsSeeder extends Seeder
         //     'updated_at' => now()
         // ]);
         Tweet::factory()->count(10)->create();
+        // ->each(
+        //     fn ($tweet) =>
+        //     Image::factory()->count(4)->create()->each(
+        //         fn ($image) =>
+        //         $tweet->images()->attach($image->id)
+        //     )
+        // );
     }
 }
